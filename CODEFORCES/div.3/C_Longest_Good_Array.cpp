16de22d26abe39d406b6cbf600ba5ec66efda8b7
babayaga-13 <muniruddinmahir@gmail.com>
2024-09-14 14:59:50 +0600

    div-3

--- CODEFORCES/div.3/C_Longest_Good_Array.cpp
+++ CODEFORCES/div.3/C_Longest_Good_Array.cpp
@@ -1,54 +1,49 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define endl "\n"
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
-#define ln cout << endl
-#define all(v) v.begin(), v.end()
-#define count_one(x) __builtin_popcount(x)
-#define trailing_zero(x) __builtin_ctz(x)
-#define leading_zero(x) __builtin_clz(x)
-#define gcd __gcd
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
-template <typename T>
-using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-const int N = 1e9 + 7;
-ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
-ll power(ll a, ll n)
+bool check(ll c, ll n, ll m)
 {
-    ll ans = 1;
-    while (n)
-    {
-        if (n % 2)
-            ans = (ans * a) % N, n--;
-        else
-            a = (a * a) % N, n /= 2;
-    }
-    return ans;
+    ll sum = (c * (c + 1)) / 2;
+    return (n + sum) <= m;
 }
 
 void solve()
 {
     ll n, m;
     cin >> n >> m;
-    ll ans = sqrt(1 + 8 * (m - n));
-    cout << (ans - 1) / 2 + 1 << endl;
+
+    ll l = 0, h = 1e5, ans = 0;
+
+    while (l <= h)
+    {
+        ll mid = (l + h) / 2;
+
+        if (check(mid, n, m))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else
+        {
+            h = mid - 1;
+        }
+    }
+
+    cout << ans + 1 << endl;
 }
 
-signed main()
+int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
-    ll t = 1;
+
+    ll t;
     cin >> t;
     while (t--)
     {
         solve();
     }
+
     return 0;
 }
