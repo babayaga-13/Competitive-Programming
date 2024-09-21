d5a5350c85c66ce495b32ebe07dbef0b00476b17
babayaga-13 <muniruddinmahir@gmail.com>
2024-09-21 19:23:29 +0600

    div-3

---
+++ CODEFORCES/div.3/D_Very_Different_Array.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int N = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % N, n--;
+        else
+            a = ((__int128_t)a * a) % N, n /= 2;
+    }
+    return ans;
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<ll> a(n), b(m);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    sort(all(a));
+    sort(rall(b));
+    ll s = 0;
+    for (int i = 0; i < n; i++)
+        s += abs(a[i] - b[i]);
+    ll ans = s, i1 = n - 1, i2 = m - 1;
+    for (int i = 0; i < n; i++)
+    {
+        s -= abs(a[i1] - b[i1]);
+        s += abs(a[i1] - b[i2]);
+        i1--, i2--;
+        ans = max(ans, s);
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
