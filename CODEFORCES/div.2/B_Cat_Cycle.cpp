7eb7cc6a905ccbfe0563e8ffababd5eb69af3a14
babayaga-13 <muniruddinmahir@gmail.com>
2024-11-15 23:59:27 +0600

    div-2

---
+++ CODEFORCES/div.2/B_Cat_Cycle.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int N = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % N, n--;
+        a = ((__int128_t)a * a) % N, n /= 2;
+    }
+    return ans;
+}
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n % 2 == 0)
+    {
+        if (k % n == 0)
+            cout << n << endl;
+        else
+            cout << k % n << endl;
+    }
+    else
+    {
+        k = k + (k - 1) / (n / 2);
+        if (k % n == 0)
+            cout << n << endl;
+        else
+            cout << k % n << endl;
+    }
+}
+// 5 1
+// 4 2
+// 3 4*
+// 2 5
+// 1 2*
+// 5 3
+// 4 5*
+// 3 1
+// 2 3*
+// 1 4
+// 5 1*
+// 4 2
+// 3 4*
+// 2 5
+// 1 2*
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
