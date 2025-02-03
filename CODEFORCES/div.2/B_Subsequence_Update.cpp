ac0f20a0b7c03c0a24d02a405dd4fe0364792928
babayaga-13 <muniruddinmahir@gmail.com>
2025-02-04 02:31:27 +0600

    div-2

---
+++ CODEFORCES/div.2/B_Subsequence_Update.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int N = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % N, n--;
+        a = ((__int128_t)a * a) % N, n /= 2;
+    }
+    return ans;
+}
+
+void solve()
+{
+    ll n, l, r;
+    cin >> n >> l >> r;
+    ll a[n + 1];
+    vector<ll> u, v;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        if (i <= r)
+            u.push_back(x);
+        if (i >= l)
+            v.push_back(x);
+    }
+    sort(all(u));
+    sort(all(v));
+    ll ans = 0, s = 0;
+    for (int i = 0; i < r - l + 1; i++)
+        ans += u[i];
+    for (int i = 0; i < r - l + 1; i++)
+        s += v[i];
+    ans = min(ans, s);
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
