bea24c4926118f81339b1797c9030434e5520bda
babayaga-13 <muniruddinmahir@gmail.com>
2025-03-07 22:12:43 +0600

    div-2

---
+++ CODEFORCES/div.2/C_Penchick_and_BBQ_Buns.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int N = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % N, n--;
+        a = ((__int128_t)a * a) % N, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        for (int i = 1; i <= n / 2; i++)
+            cout << i << " " << i << " ";
+        ln;
+    }
+    else
+    {
+        if (n < 27)
+        {
+            cout << "-1\n";
+        }
+        else
+        {
+            int c = 1, x = 3;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1 || i == 10 || i == 26)
+                    cout << "1 ";
+                else if (i == 11 || i == 27)
+                    cout << "2 ";
+                else
+                {
+                    cout << x << " ";
+                    c++;
+                    if (c & 1)
+                        x++;
+                }
+            }
+            ln;
+        }
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
