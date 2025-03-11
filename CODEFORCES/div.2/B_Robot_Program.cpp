eeaf3c414ca35aa25683e5b659fdbea34a96e8f6
babayaga-13 <muniruddinmahir@gmail.com>
2025-03-11 23:00:40 +0600

    div-3

---
+++ CODEFORCES/div.2/B_Robot_Program.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+void solve()
+{
+    ll n, x, k, c = 0, ans = 0;
+    cin >> n >> x >> k;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'L')
+            x--;
+        else
+            x++;
+        k--;
+
+        if (x == 0)
+        {
+            ans++;
+            break;
+        }
+    }
+    if (x != 0)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'L')
+            x--;
+        else
+            x++;
+        c++;
+        if (x == 0)
+        {
+            ans += (k / c);
+            break;
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
