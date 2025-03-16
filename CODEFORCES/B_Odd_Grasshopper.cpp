356f93fdb05528881c4961283b0ff04f35fac90b
babayaga-13 <muniruddinmahir@gmail.com>
2025-03-16 23:12:52 +0600

    cf

---
+++ CODEFORCES/B_Odd_Grasshopper.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(y) __builtin_popcount(y)
+#define trailing_zero(y) __builtin_ctz(y)
+#define leading_zero(y) __builtin_clz(y)
+#define gcd __gcd
+using namespace std;
+
+const int MOD = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+void solve()
+{
+    ll m, n;
+    cin >> m >> n;
+    ll y = n % 4;
+    ll x = n - n % 4;
+    for (ll i = 1; i <= y; i++)
+    {
+        if (m % 2)
+        {
+            m += (i + x);
+        }
+        else
+        {
+            m -= (i + x);
+        }
+    }
+    cout << m << "\n";
+}
+
+signed
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
