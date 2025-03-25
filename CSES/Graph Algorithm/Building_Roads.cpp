389d01d5cc93e2c2e9804b812f642da2eb59c77c
babayaga-13 <muniruddinmahir@gmail.com>
2025-03-25 19:42:01 +0600

    graph

---
+++ CSES/Graph Algorithm/Building_Roads.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+vector<vector<int>> adj;
+vector<bool> vis;
+
+void dfs(int u)
+{
+    vis[u] = true;
+    for (int v : adj[u])
+    {
+        if (!vis[v])
+        {
+            dfs(v);
+        }
+    }
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    adj.resize(n + 1);
+    vis.assign(n + 1, false);
+
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    vector<int> ans;
+    for (int u = 1; u <= n; ++u)
+    {
+        if (!vis[u])
+        {
+            ans.push_back(u);
+            dfs(u);
+        }
+    }
+
+    int k = ans.size() - 1;
+    cout << k << endl;
+
+    for (int i = 1; i < ans.size(); ++i)
+    {
+        cout << ans[i - 1] << " " << ans[i] << endl;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ CSES/Graph Algorithm/Counting_Rooms.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+int n, m;
+char a[1000][1000];
+int vis[1000][1000] = {0};
+bool check(int i, int j)
+{
+    if (i < 0 || i > n - 1 || j < 0 || j > m - 1 || a[i][j] == '#')
+        return false;
+    return true;
+}
+void dfs(int x, int y)
+{
+    vis[x][y] = 1;
+    if (!vis[x][y + 1] && check(x, y + 1))
+        dfs(x, y + 1);
+    if (!vis[x][y - 1] && check(x, y - 1))
+        dfs(x, y - 1);
+    if (!vis[x + 1][y] && check(x + 1, y))
+        dfs(x + 1, y);
+    if (!vis[x - 1][y] && check(x - 1, y))
+        dfs(x - 1, y);
+}
+void solve()
+{
+    cin >> n >> m;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (!vis[i][j] && a[i][j] == '.')
+            {
+                dfs(i, j);
+                ans++;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
