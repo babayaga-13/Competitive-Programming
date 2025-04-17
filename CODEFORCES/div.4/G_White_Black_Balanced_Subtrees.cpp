21e42148b76fd390b9999d12f17ba51ce223255d
babayaga-13 <muniruddinmahir@gmail.com>
2025-04-18 00:21:16 +0600

    div-4

---
+++ CODEFORCES/div.4/G_White_Black_Balanced_Subtrees.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+string s;
+int ans = 0;
+vector<vector<int>> v;
+pair<int, int> dfs(int x)
+{
+    int a = 0, b = 0;
+    for (auto u : v[x])
+    {
+        auto [wa, wb] = dfs(u);
+        a += wa;
+        b += wb;
+    }
+    if (s[x - 1] == 'W')
+        a++;
+    else
+        b++;
+    if (a == b)
+        ans++;
+    return {a, b};
+}
+
+void solve()
+{
+    ans = 0;
+    int n, a, b;
+    cin >> n;
+    v.clear();
+    v.resize(n + 1);
+    for (int i = 2; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        v[x].push_back(i);
+    }
+    cin >> s;
+    dfs(1);
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
