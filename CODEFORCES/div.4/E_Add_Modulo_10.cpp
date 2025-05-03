c5d1a53f1003a9251d35a12a4a6f54ec4bab297c
babayaga-13 <muniruddinmahir@gmail.com>
2025-05-03 18:00:59 +0600

    div-4

---
+++ CODEFORCES/div.4/E_Add_Modulo_10.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    set<int> s;
+    int a[n];
+    bool f = false;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] % 10 == 5)
+            a[i] += 5;
+        if (a[i] % 10 == 0)
+            f = true;
+        s.insert(a[i]);
+    }
+    if (s.size() == 1)
+    {
+        yes;
+        return;
+    }
+
+    if (f)
+    {
+        no;
+        return;
+    }
+    s.clear();
+    for (int i = 0; i < n; i++)
+    {
+        while (a[i] % 10 != 2)
+        {
+            a[i] += (a[i] % 10);
+        }
+        if (a[i] % 20 == 2)
+            s.insert(1);
+        else
+            s.insert(2);
+        if (s.size() == 2)
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
