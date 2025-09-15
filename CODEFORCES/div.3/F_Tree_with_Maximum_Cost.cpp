4fa9d6ab220e2f567818ef2d1af689852cf28d95
babayaga-13 <muniruddinmahir@gmail.com>
2025-09-15 20:34:45 +0600

    div-3

---
+++ CODEFORCES/div.3/F_Tree_with_Maximum_Cost.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define vi vector<int>
+#define vpi vector<pi>
+#define vii vector<vector<int>>
+#define pi pair<int, int>
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctzll(x)
+#define leading_zero(x) __builtin_clzll(x)
+#define gcd __gcd
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+long long fact[N], inv_fact[N];
+void init_fact()
+{
+    fact[0] = inv_fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+    inv_fact[N - 1] = power(fact[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 1; i--)
+    {
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
+    }
+}
+int nCr(int n, int r)
+{
+    return fact[n] % MOD * inv_fact[r] % MOD * inv_fact[n - r] % MOD;
+}
+long long nPr(int n, int r)
+{
+    return fact[n] * inv_fact[n - r] % MOD;
+}
+
+int n;
+vi a, b, ans;
+vii adj;
+
+void dfs(int c, int p)
+{
+    b[c] = a[c];
+    for (auto u : adj[c])
+    {
+        if (u != p)
+        {
+            dfs(u, c);
+            b[c] += (2 * b[u]);
+        }
+    }
+}
+void rdfs(int c, int p)
+{
+    b[c] = a[c];
+    for (auto u : adj[c])
+    {
+        if (u != p)
+        {
+            dfs(u, c);
+            b[c] += (2 * b[u]);
+        }
+    }
+}
+void solve()
+{
+    cin >> n;
+    a.assign(n + 1, 0);
+    b.assign(n + 1, 0);
+    ans.assign(n + 1, 0);
+    adj.assign(n + 1, {});
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    for (int i = 1; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+
+    dfs(1, 0);
+    ans[1] = b[1];
+    rdfs(1, 0);
+    int total = 0;
+    for (auto u : adj[1])
+        cout << b[u] << " ";
+    cout << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
