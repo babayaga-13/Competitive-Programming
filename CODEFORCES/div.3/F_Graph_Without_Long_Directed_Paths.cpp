cb8e049186cc6e77fd9b9b5c6010f3884c27abcb
babayaga-13 <muniruddinmahir@gmail.com>
2025-09-28 23:36:55 +0600

    div-3

---
+++ CODEFORCES/div.3/F_Graph_Without_Long_Directed_Paths.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define vi vector<int>
+#define vpi vector<pi>
+#define vii vector<vector<int>>
+#define pi pair<int, int>
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <typename T>
+// using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+long long fact[N], inv_fact[N];
+void init_fact()
+{
+    fact[0] = inv_fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+    inv_fact[N - 1] = power(fact[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 1; i--)
+    {
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
+    }
+}
+int nCr(int n, int r)
+{
+    return fact[n] % MOD * inv_fact[r] % MOD * inv_fact[n - r] % MOD;
+}
+long long nPr(int n, int r)
+{
+    return fact[n] * inv_fact[n - r] % MOD;
+}
+int n, m;
+vii adj;
+vi vis, c;
+vector<pi> v;
+bool dfs(int p, int f)
+{
+    vis[p] = 1;
+    c[p] = f;
+    for (auto u : adj[p])
+    {
+        if (!vis[u])
+        {
+            if (!dfs(u, -f))
+                return false;
+        }
+        else if (c[u] == c[p])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve()
+{
+    cin >> n >> m;
+    adj.assign(n + 1, {});
+    vis.assign(n + 1, 0);
+    c.assign(n + 1, 0);
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    if (!dfs(1, 1))
+    {
+        no;
+    }
+    else
+    {
+        yes;
+        for (auto [i, j] : v)
+        {
+            if (c[i] == 1)
+                cout << 1;
+            else
+                cout << 0;
+            // cout << c[i] << " " << c[j] << endl;
+        }
+        ln;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
