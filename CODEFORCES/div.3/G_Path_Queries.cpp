3a3738da4a0f0f7f06a1b06d36740b2e2cf1b9f1
babayaga-13 <muniruddinmahir@gmail.com>
2025-10-10 03:32:01 +0600

    div-3

---
+++ CODEFORCES/div.3/G_Path_Queries.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define vi vector<int>
+#define vpi vector<pi>
+#define vii vector<vector<int>>
+#define pi pair<int, int>
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <typename T>
+// using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+long long fact[N], inv_fact[N];
+void init_fact()
+{
+    fact[0] = inv_fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+    inv_fact[N - 1] = power(fact[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 1; i--)
+    {
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
+    }
+}
+int nCr(int n, int r)
+{
+    return fact[n] % MOD * inv_fact[r] % MOD * inv_fact[n - r] % MOD;
+}
+long long nPr(int n, int r)
+{
+    return fact[n] * inv_fact[n - r] % MOD;
+}
+struct DSU
+{
+    vector<int> parent, sz;
+    DSU(int n = 0)
+    {
+        parent.assign(n + 1, 0);
+        sz.assign(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+        {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    int find(int x)
+    {
+        if (parent[x] == x)
+            return x;
+        else
+            return parent[x] = find(parent[x]);
+    }
+    bool unite(int a, int b)
+    {
+        a = find(a);
+        b = find(b);
+        if (a == b)
+            return false;
+        if (sz[a] < sz[b])
+            swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+        return true;
+    }
+};
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vii v(n - 1, vi(3));
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> v[i][1] >> v[i][2] >> v[i][0];
+    }
+    vector<pi> q;
+    vi a(m);
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        q.push_back({x, i});
+    }
+    sort(all(q));
+    sort(all(v));
+    int ans = 0, j = 0;
+    DSU d(n);
+    for (int i = 0; i < m; i++)
+    {
+        while (j < n - 1 && v[j][0] <= q[i].first)
+        {
+            int x = d.find(v[j][1]);
+            int y = d.find(v[j][2]);
+            if (x != y)
+                ans += (d.sz[x] * d.sz[y]);
+            d.unite(v[j][1], v[j][2]);
+            j++;
+        }
+        a[q[i].second] = ans;
+    }
+    for (int i = 0; i < m; i++)
+        cout << a[i] << " ";
+    ln;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
