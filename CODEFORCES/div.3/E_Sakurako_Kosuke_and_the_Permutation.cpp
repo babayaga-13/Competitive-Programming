a040b90125c022067dc32a2b48c757e4d0618eaf
babayaga-13 <muniruddinmahir@gmail.com>
2025-10-12 23:55:34 +0600

    div-3

--- CODEFORCES/div.3/E_Sakurako_Kosuke_and_the_Permutation.cpp
+++ CODEFORCES/div.3/E_Sakurako_Kosuke_and_the_Permutation.cpp
@@ -1,47 +1,112 @@
 #include <bits/stdc++.h>
+#define int long long
 #define ll long long
+#define vi vector<int>
+#define vpi vector<pi>
+#define vii vector<vector<int>>
+#define pi pair<int, int>
 #define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
 using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <typename T>
+// using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = ((__int128_t)ans * a) % MOD, n--;
+        a = ((__int128_t)a * a) % MOD, n /= 2;
+    }
+    return ans;
+}
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+long long fact[N], inv_fact[N];
+void init_fact()
+{
+    fact[0] = inv_fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+    inv_fact[N - 1] = power(fact[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 1; i--)
+    {
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
+    }
+}
+int nCr(int n, int r)
+{
+    return fact[n] % MOD * inv_fact[r] % MOD * inv_fact[n - r] % MOD;
+}
+long long nPr(int n, int r)
+{
+    return fact[n] * inv_fact[n - r] % MOD;
+}
 
 void solve()
 {
     int n;
     cin >> n;
-    vector<int> p(n + 1); // Using 1-based index
-    for (int i = 1; i <= n; i++)
-        cin >> p[i];
-
-    vector<bool> visited(n + 1, false);
-    int swaps = 0;
-
+    vi a(n + 1, 0);
+    for (int i = 0; i < n; i++)
+        cin >> a[i + 1];
+    vi vis(n + 1, 0);
+    vis[0] = 1;
+    int ans = 0;
     for (int i = 1; i <= n; i++)
     {
-        if (!visited[i])
+        int c = 0, x = a[i];
+        while (!vis[x])
         {
-            int cycle_size = 0;
-            int x = i;
-            while (!visited[x])
-            {
-                visited[x] = true;
-                x = p[x];
-                cycle_size++;
-            }
-            // If the cycle size is greater than 1, we can fix it with (cycle_size - 1) swaps
-            if (cycle_size > 1)
-            {
-                swaps += (cycle_size - 1);
-            }
+            c++;
+            vis[x] = 1;
+            x = a[x];
         }
+        if (c > 1)
+            ans += (c - 1) / 2;
     }
-
-    cout << swaps << endl;
+    cout << ans << endl;
 }
 
 signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
+    cout.tie(NULL);
+    ll t = 1;
     cin >> t;
     while (t--)
     {
